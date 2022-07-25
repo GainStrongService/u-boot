d04f375ae6a6a915c18514bd92759493f30da744
Weijie Gao <weijie.gao@mediatek.com>
2022-07-25 17:13:32 +0800

    boot: add support to force FIT image verification for bootm
    Add a config to allow force FIT image verification for bootm when FIT image
    signing is enabled for secure boot.
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- boot/bootm.c
+++ boot/bootm.c
@@ -72,7 +72,11 @@ static int bootm_start(struct cmd_tbl *cmdtp, int flag, int argc,
 		       char *const argv[])
 {
 	memset((void *)&images, 0, sizeof(images));
+#if defined(CONFIG_IMAGE_FORCED_VERIFY)
+	images.verify = 1;
+#else
 	images.verify = env_get_yesno("verify");
+#endif
 
 	boot_start_lmb(&images);
 
