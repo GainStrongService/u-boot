3ac61e5827339bd46dc1282107b14becb3e38cbe
Weijie Gao <weijie.gao@mediatek.com>
2022-07-27 16:32:17 +0800

    mtd: spi-nor: add more flash ids
    Add more spi-nor flash ids
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- drivers/mtd/spi/spi-nor-core.c
+++ drivers/mtd/spi/spi-nor-core.c
@@ -648,6 +648,7 @@ static int set_4byte(struct spi_nor *nor, const struct flash_info *info,
 	case SNOR_MFR_ISSI:
 	case SNOR_MFR_MACRONIX:
 	case SNOR_MFR_WINBOND:
+	case SNOR_MFR_EON:
 		if (need_wren)
 			write_enable(nor);
 
